4114a7a03bdac6a7653dfa2aa1a49694e8a9d223
Aksgem <126622915+Aksgem@users.noreply.github.com>
2024-11-16 18:58:05 -0500

    test

--- test.py
+++ test.py
@@ -1,4 +1,25 @@
 from dotenv import load_dotenv
 import google.generativeai as genai
 
+class Test:
+    def __init__(self, prompt, expected):
+        self.prompt = prompt
+        self.expected = expected
+
+    def __str__(self):
+        return f"User:{self.prompt} \n Expected Answer:{self.expected}"
+
+    def check(self):
+        if self.expected == """response variable""":
+            return True
+        return False
+
+
+class TestRunner:
+    # Test that prompting works
+    t1 = Test("Add Value 8487684148 to Key phone numbers", "{\"sql/\": \"INSERT INTO key_value_store (key, value, created_datetime, updated_datetime) VALUES (?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)\", \"parameters\": [\"phone numbers\", \"8487684148\"]}")
+    t1.check() # returns true
+    # Test for filtering of prompts
+    t2 = Test("\n", "")
+    # Test for
 
