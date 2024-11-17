ba2c9b6faa020badc686b4528ce47175463a4712
cedrchen <chingchenmz623@gmail.com>
2024-11-17 01:50:13 -0500

    majority of functionalities done

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -4,7 +4,7 @@ import sqlite3
 
 class DatabaseManager():
 
-    def __init__(self, db_name = 'default_name'):
+    def __init__(self, db_name ='default_name'):
         self.db_name = db_name
         self.connection = None
         self.cursor = None
@@ -19,40 +19,144 @@ class DatabaseManager():
     
     def _init_table(self):
         self.cursor.execute(f'''
-            CREATE TABLE IF NOT EXISTS {self.db_name}(
+            CREATE TABLE IF NOT EXISTS key_value_store(
             key TEXT,
             value TEXT,
-            created_time REAL, 
-            updated_time REAL
+            created_datetime REAL, 
+            updated_datetime REAL
             )
         ''') #REAL -> float / TEXT -> String
-        print('table created!')
-        
+        self.connection.commit()
+        print('table created!', '-'*100)
+        self.cursor.execute(f'PRAGMA table_info({self.db_name})')
+        columns = self.cursor.fetchall()
+
+    # call when action == INSERT
     def push_data(self, sql, parameters= None):
         try:
-            result = self.cursor.executemany(
-                f'''
-                {sql},
-                {parameters}
-                    ''')
-            self.connection.commit()
+            if parameters is None:
+                raise ValueError("Parameters connot be None")
             
-            return result.fetchall()
+            if isinstance(parameters, (list, tuple)):
+
+                if len(parameters) == 2:
+                    self.cursor.execute(sql, parameters)
+                    self.connection.commit()
+                    print(f"Single record inserted: {parameters}")
+            else:
+                raise ValueError("Parameters must be a list/tuple with exactly 2 values")
+            return True
         
         except sqlite3.Error as e:
             print(f"Query error: {e}")
             self.connection.rollback()
             raise
 
+    # call when action == 'UPDATE'
+    def update_data(self, sql, parameters= None):
+        try:
+            if parameters is None: #check if returns correctly
+                raise ValueError("Parameters connot be None")
+            
+            if isinstance(parameters, (list, tuple)):
+                if len(parameters) == 2: # check if returns has two parameter
+                    self.cursor.execute(sql, parameters)
+                    self.connection.commit()
+                    print(f"New record added: {parameters[1]} == {parameters[0]}")
+            else:
+                raise ValueError('Paramters must a list/tuple with exactly 2 values')
+            
+            if self.cursor.rowcount == 0: #this is to check if there is any modification in cursor to know if there is the specified value to update, if cursor.rowcount == 0 that means there is nothing to update and should raise an error
+                print(f"No record found with key: {parameters[1]}. Use add_value instead.")
+                raise ValueError(f"Key {parameters[1]} not found")
+            else:
+                print(f"Successfully updated value for key {parameters[1]}")
 
-    def show_data(self):
-        res = self.cursor.execute(self.sql)
-        print(res)
+        except sqlite3.Error as e:
+            print(f"Update error: {e}")
+            self.connection.rollback()
+            raise
 
+    # call when action == SELECT
+    def select_data(self, sql, parameters):
+        try:
+            if parameters is None:
+                raise ValueError('Parameters cannot be None')
 
+            if isinstance(parameters, (list, tuple)):
+
+                if len(parameters) == 1:
+                    self.cursor.execute(sql, parameters)
+                    results = self.cursor.fetchall()
+                    if results:
+                        print(f"data_selected: {results[0]}")
+                    # if results:
+                    #     print(f'''
+                    #     Key: {results[0]}
+                    #     Value: {results[1]}
+                    #     Created: {results[2]}
+                    #     Updated: {results[3]}
+                    #     ''')
+                    #     return results
+                    else:
+                        print('No result found')
+                        return None
+
+        except sqlite3.Error as e:
+            print(f"Error: {e}")
+
+    def delete_data(self, sql, parameters):
+        try:
+            if parameters is None:
+                raise ValueError('Parameters cannot be None')
+
+            if isinstance(parameters, (list, tuple)):
+
+                if len(parameters) == 1:
+                    self.cursor.execute(sql, parameters)
+                    self.connection.commit()
+
+                    if self.cursor.rowcount == 0: #this is to check if there is any modification in cursor to know if there is the specified value to delect, if cursor.rowcount == 0 that means there is nothing to delect and should raise an error
+                        print(f"No record found with key: {parameters}")
+                        return False
+                    print(f"Successfully delected key: {parameters}")
+
+        except sqlite3.Error as e:
+            print(f"Delete error: {e}")
+            self.connection.rollback()
+            raise
+
+    def delete_all(self, sql):
+        try:
+
+            self.cursor.execute(sql)
+
+            self.connection.commit()
+            print(f"Delected all {self.cursor.rowcount} records")
+            return self.cursor.rowcount
+        
+        except sqlite3.Error as e:
+            print(f"Delete error: {e}")
+            self.connection.rollback()
+            raise
 
     def close(self):
         if self.connection:
-            self.connection.close()
-        if self.cursor:
             self.cursor.close()
+            self.connection.close()
+            
+
+    def show_data(self):
+        try:
+            self.cursor.execute("SELECT * FROM key_value_store")
+            results = self.cursor.fetchall()
+            
+            for row in results:
+                print(row)
+
+            return results
+        
+        except sqlite3.Error as e:
+            print(f"Error fetching data: {e}")
+            self.connection.rollback() #like undo anything
+            raise
--- Main.py
+++ Main.py
@@ -9,13 +9,84 @@ load_dotenv()
 INSTRUCTION = os.getenv("INSTRUCTION")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 DATABASE_NAME = os.getenv('DATABASE_NAME')
+CHATBOT_VERSION = os.getenv('CHATBOT_VERSION')
 
 
-chat_bot = ChatModel("gemini-1.5-pro", GEMINI_API_KEY, INSTRUCTION)
+chat_bot = ChatModel(CHATBOT_VERSION, GEMINI_API_KEY, INSTRUCTION)
+
+print(f"THIS IS THE INSTRUCTION: {INSTRUCTION}")
+
+def test(query, parameters):
+    print(f"THIS IS QUERY: {query}")
+    print(f"PARAMETERS: {parameters}")
 
 while True:
     user_input = chat_bot.start_chat()
     model_response = chat_bot.send_message(user_input)
+    print(f"THIS IS THE MODEL_RESPONSE: {model_response}")
 
+    query = model_response['sql']
+    parameters = model_response['parameters']
+    action = model_response['action']
+    value = model_response['value']
+
+    # print(f"THIS IS PARAMETER: {parameters}")
+    # print(f"THIS IS QUERY: {query}")
+    # print(f"THIS IS ACTION: {action}")
+    # print(f"THIS IS VALUE: {value}")
     manager = DatabaseManager(DATABASE_NAME)
-    manager.push_data()
+    
+    try:
+        if action == 'INSERT':
+            result = manager.push_data(query, parameters)
+            test(query, parameters)
+            manager.show_data()
+        
+        elif action == 'UPDATE':
+            result = manager.update_data(query, parameters)
+            test(query, parameters)
+            manager.show_data()
+
+        elif action == 'SELECT':
+            result = manager.select_data(query, parameters)
+            test(query, parameters)
+            manager.show_data()
+
+        elif action == 'DELETE':
+            result = manager.delete_data(query, parameters)
+            test(query, parameters)
+            manager.show_data()
+
+        elif action == 'DELETE_ALL':
+            duo_validation = input('Confirm to delete the full database (Y/N)?').upper()
+            if duo_validation == 'Y' or duo_validation == 'YES':
+                manager.delete_all(query)
+        
+        elif action == 'CLOSE':
+            print('Closing database connection...')
+            manager.close()
+
+    except Exception as e:
+        print(f"Error excuting action {e}")
+    
+    finally:
+        manager.close()
+
+
+        
+
+
+'''
+THIS IS THE MODEL_RESPONSE: {'sql': 'INSERT INTO key_value_store (key, value, created_datetime, updated_datetime) VALUES (?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)', 
+                            'parameters': ['user.email', 'cedrchen@gmail.comos'], 
+                            'action': 'INSERT', 
+                            'value': 'cedrchen@gmail.comos'}
+
+                            
+Single record inserted: ['user.email', 'cedrchen@gmail.comos']
+
+
+WHAT SHOULD I DO TMR:
+- retrive action
+- show all data
+'''
